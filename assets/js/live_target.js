c6e758b54d19fe090ff25843d4985305825d5710
dannysun91 <dannysun@berkeley.edu>
2026-04-30 10:46:16 -0800

    picking up count will shake if yes

--- assets/js/live_target.js
+++ assets/js/live_target.js
@@ -2,15 +2,22 @@ function fetchLetter() {
     fetch("https://foxsi.dannysun.workers.dev")
         .then(res => res.text())
         .then(text => {
-            document.getElementById("my-letter").textContent = text;
+            if (text==""){
+                document.getElementById("my-letter").textContent = "";
+            }
+            else{
+                document.getElementById("my-letter").textContent = `Live Target: ${text}`;
+            }
 
             if (text==""){
                 document.getElementById("picking-up-count").textContent = "No";
                 document.getElementById("picking-up-count").style.color = "red";
+                document.getElementById("picking-up-count").classList.remove('shake');
             }
             else{
                 document.getElementById("picking-up-count").textContent = "YES";
                 document.getElementById("picking-up-count").style.color = "green";
+                document.getElementById("picking-up-count").classList.add('shake');
             }
         });
 }
