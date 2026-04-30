c898d363389daca9208841d4451594d346affe9f
dannysun91 <70768517+dannysun91@users.noreply.github.com>
2026-04-29 17:45:45 -0700

    using a throwaway token

--- assets/js/live_target.js
+++ assets/js/live_target.js
@@ -1,19 +1,29 @@
-function fetchLetter() {
-    fetch("https://foxsi.dannysun.workers.dev")
-        .then(res => res.text())
-        .then(text => {
-            document.getElementById("my-letter").textContent = text;
+const GIST_ID = "fce9b41b568ed17aa79d06b7c613a260";
+const FILENAME = "foxsi5.txt";
+const TOKEN = "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";
 
-            if (text==""){
-                document.getElementById("picking-up-count").textContent = "No";
-                document.getElementById("picking-up-count").style.color = "red";
-            }
-            else{
-                document.getElementById("picking-up-count").textContent = "YES";
-                document.getElementById("picking-up-count").style.color = "green";
-            }
-        });
+function fetchLetter() {
+    fetch(`https://api.github.com/gists/${GIST_ID}`, {
+        headers: {
+        "Authorization": `Bearer ${TOKEN}`,
+        "X-GitHub-Api-Version": "2022-11-28"
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+        const text = data.files[FILENAME].content;
+        document.getElementById("my-letter").textContent = text.trim();
+        
+        if (text.trim()==""){
+            document.getElementById("picking-up-count").textContent = "No";
+            document.getElementById("picking-up-count").style.color = "red";
+        }
+        else{
+            document.getElementById("picking-up-count").textContent = "YES";
+            document.getElementById("picking-up-count").style.color = "green";
+        }
+    });
 }
 
-fetchLetter();
-setInterval(fetchLetter, 2000);
+fetchLetter();                  // run immediately on load
+setInterval(fetchLetter, 1000);
