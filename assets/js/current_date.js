21446e95493bd7dc2c997f276e43c1b83c1a54e2
dannysun91 <dannysun@berkeley.edu>
2026-04-30 10:00:22 -0800

    updated for 4/30

--- assets/js/current_date.js
+++ assets/js/current_date.js
@@ -19,3 +19,4 @@ function getOrdinal(day) {
     
     
 document.getElementById('curr_date').textContent = `${month} ${day}, ${y}`;
+document.getElementById('hmi-img').src = `/foxsi5/foxsi5_launch_days/foxsi5_daily_continuum/HMI_CONT_IMAGE_2026-${m}-${d}.png`;
